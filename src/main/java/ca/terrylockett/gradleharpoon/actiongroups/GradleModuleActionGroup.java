782c5186659a2c2897d5ab6b0850acc3edcacf3a
Terry Lockett <5551818+terrylockett@users.noreply.github.com>
2023-12-15 11:24:31 -0400

    fixing project path for nested subprojects

--- src/main/java/ca/terrylockett/gradleharpoon/actiongroups/GradleModuleActionGroup.java
+++ src/main/java/ca/terrylockett/gradleharpoon/actiongroups/GradleModuleActionGroup.java
@@ -7,11 +7,13 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.gradle.settings.GradleExtensionsSettings;
 import org.jetbrains.plugins.gradle.settings.GradleSettings;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 public class GradleModuleActionGroup extends ActionGroup {
 
@@ -51,18 +53,14 @@ public class GradleModuleActionGroup extends ActionGroup {
 		return name;
 	}
 	
-	public static List<String> getModulePaths(AnActionEvent e) {
+	public static Set<String> getModulePaths(AnActionEvent e) {
 		var project = e.getProject();
 		GradleSettings gradleSettings = GradleSettings.getInstance(project);
+		var gradleProjectSettings = gradleSettings.getLinkedProjectsSettings().stream().findFirst().get();
+		var gradleExtensionSettings = GradleExtensionsSettings.getInstance(e.getProject());
+		var rootGradleProject = gradleExtensionSettings.getRootGradleProject(gradleProjectSettings.getExternalProjectPath());
 
-		var list = gradleSettings.getLinkedProjectsSettings();
-		var config = list.stream().findFirst().get();
-
-		String basePath = gradleSettings.getProject().getBasePath();
-		String baseName = Arrays.stream(basePath.split("/")).reduce((first, second) -> second).orElse(null);
-		config.getModules();
-
-		return new ArrayList<>(config.getModules());
+		return rootGradleProject.extensions.keySet();
 	}
 
 }
